9c0bc764b174973ffe530e6575fb694d8b8d8108
JohnFGC <jfgc2002@gmail.com>
2022-11-10 17:20:49 -0500

    kill me

--- src/main/java/pizzaManager/Order.java
+++ src/main/java/pizzaManager/Order.java
@@ -1,13 +1,49 @@
 package pizzaManager;
 
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
 public class Order implements Customizable{
+    private int serialNumber;
+    private static final double TAX_RATE = 0.06625;
+    private ArrayList <Pizza> pizzaList;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public Order (int serialNumber){
+        this.serialNumber = serialNumber;
+        this.pizzaList = new ArrayList<Pizza>();
+    }
+
+    public double getSubtotal(){
+        double subTotal = 0;
+        for (Pizza pizza: pizzaList){
+            subTotal += pizza.price();
+        }
+        return subTotal;
+    }
+
+    public double getTax(){
+        double tax = getSubtotal() * TAX_RATE;
+        return Double.parseDouble(df.format(tax));
+    }
+
+    public double getTotal(){
+        return getTax() + getSubtotal();
+    }
+
     @Override
     public boolean add(Object obj) {
+        if (obj instanceof Pizza){
+            return pizzaList.add((Pizza) obj);
+        }
         return false;
     }
 
     @Override
     public boolean remove(Object obj) {
+        if (obj instanceof Pizza){
+            return pizzaList.remove((Pizza) obj);
+        }
         return false;
     }
 }
