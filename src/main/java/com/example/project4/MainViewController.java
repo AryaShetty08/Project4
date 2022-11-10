d1874e7557d8e0ab804474c750b27a851bf36fef
AryaShetty08 <arya.shetty08@gmail.com>
2022-11-09 23:19:36 -0500

    Added things to project 4

--- src/main/java/com/example/project4/MainViewController.java
+++ src/main/java/com/example/project4/MainViewController.java
@@ -1,4 +1,25 @@
 package com.example.project4;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class MainViewController {
+    @FXML
+    private ImageView chicagoPizzaImage;
+    @FXML
+    private ImageView NYPizzaImage;
+    @FXML
+    private ImageView StoreOrdersImage;
+    @FXML
+    private ImageView CurrentOrderImage;
+
+    @FXML
+    public void initialize() {
+        chicagoPizzaImage.setImage(new Image("chicagopizzaimage.jpg"));
+        NYPizzaImage.setImage(new Image("NYPizzaImage.jpg"));
+        StoreOrdersImage.setImage(new Image("StoreOrdersImage.jpg"));
+        CurrentOrderImage.setImage(new Image("currentorderimage.jpg"));
+    }
+
 }
