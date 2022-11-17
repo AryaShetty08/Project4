3075f32bd9a373eb6873bbc0570642552b97e050
AryaShetty08 <arya.shetty08@gmail.com>
2022-11-17 00:37:16 -0500

    Added things to project 4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.project4 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires org.testng;
+    requires junit;
 
 
     opens com.example.project4 to javafx.fxml;
---
+++ src/main/java/pizzaManager/BuildYourOwnTest.java
@@ -0,0 +1,37 @@
+package pizzaManager;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertFalse;
+import static pizzaManager.Size.*;
+
+class BuildYourOwnTest {
+
+    @org.junit.Test
+    void add() {
+
+    }
+
+    @org.junit.Test
+    void remove() {
+    }
+
+    @org.junit.Test
+    void price() {
+        Size crustType[] = {SMALL, MEDIUM, LARGE};
+
+
+        for (int i = 0; i < crustType.length; i++) {
+
+            //assertFalse(price().isValid());
+        }
+    }
+
+    @org.junit.Test
+    void setSize() {
+    }
+
+    @org.junit.Test
+    void testToString() {
+    }
+}
